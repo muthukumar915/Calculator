2b5dd7a1ceece0624efeb78955a536068b44e4c8
Muthukumar455 <rmuthukumar455@gmail.com>
2024-06-17 16:44:20 +0530

    file added

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// to get inupt data
 var a=document.getElementById("display");
 
+// add number to input
 function appendDisplay(num){
     a.value+=num;
     // console.log(a)
 }
+
+// clear function
 function appendClear(){
     a.value="";
 }
+// eval is used to for calculation
 function appendtoResult(){
     a.value=eval(a.value);
 }
